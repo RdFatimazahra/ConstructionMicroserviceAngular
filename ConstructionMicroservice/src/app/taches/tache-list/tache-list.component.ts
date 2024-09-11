1632da28274c1869582e137f5ed16e53e21bbffa
ilyashq <ilyasenaa24@gmail.com>
2024-09-11 19:03:08 +0100

    Add TacheListComponent for task management
    - Implemented TacheListComponent to display and manage task lists.
    - Integrated with TaskService for CRUD operations.

--- ConstructionMicroservice/src/app/taches/tache-list/tache-list.component.ts
+++ ConstructionMicroservice/src/app/taches/tache-list/tache-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TacheService } from '../../Service/tache.service';
+import { Tache } from '../../interfaces/tache';
 
 @Component({
   selector: 'app-tache-list',
   templateUrl: './tache-list.component.html',
-  styleUrls: ['./tache-list.component.scss']
+  styleUrls: ['./tache-list.component.css']
 })
-export class TacheListComponent {
+export class TacheListComponent implements OnInit {
+  taches: Tache[] = [];
 
+  constructor(private tacheService: TacheService) { }
+
+  ngOnInit(): void {
+    this.fetchTaches();
+  }
+
+  fetchTaches() {
+    this.tacheService.getAllTaches().subscribe((data: Tache[]) => {
+      this.taches = data;
+    });
+  }
+
+  deleteTache(id: number) {
+    this.tacheService.deleteTache(id).subscribe(() => {
+      this.fetchTaches();
+    });
+  }
 }
