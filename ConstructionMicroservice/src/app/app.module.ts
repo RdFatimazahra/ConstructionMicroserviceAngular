1d8e374a60d49ef0709f4b75189be4d110295512
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-09-11 12:11:47 +0100

    Create components list, add and edit projet

--- ConstructionMicroservice/src/app/app.module.ts
+++ ConstructionMicroservice/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListProjetComponent } from './projets/add-projet/list-projet/list-projet.component';
+import { AddProjetComponent } from './projets/add-projet/add-projet/add-projet.component';
+import { EditProjetComponent } from './projets/add-projet/edit-projet/edit-projet.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProjetComponent,
+    AddProjetComponent,
+    EditProjetComponent
   ],
   imports: [
     BrowserModule,
